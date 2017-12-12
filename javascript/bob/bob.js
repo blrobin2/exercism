e62eb85bd2c601f56d9fe551cd2c5bb23f42b5cd
Bennie <brobinson88@gmail.com>
2017-12-12 15:46:44 -0500

    Initial bob solution

---
+++ javascript/bob/bob.js
@@ -0,0 +1,24 @@
+module.exports = class Bob {
+  hey(greeting) {
+    const trimmedGreeting = greeting.trim();
+    if (!trimmedGreeting) {
+      return 'Fine. Be that way!';
+    }
+    if (this._isAllUpperCase(trimmedGreeting)) {
+      return 'Whoa, chill out!';
+    }
+    if (this._endsInQuestionMark(trimmedGreeting)) {
+      return 'Sure.';
+    }
+    return 'Whatever.';
+  }
+
+  _isAllUpperCase(greeting) {
+    return greeting.match(/[A-Z]/g)
+      && greeting === greeting.toUpperCase()
+  }
+
+  _endsInQuestionMark(greeting) {
+    return greeting.match(/\?$/g);
+  }
+}
