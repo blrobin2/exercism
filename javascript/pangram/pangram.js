d2aab017c878ea6ce324b30024cd01b542f249c1
Bennie <brobinson88@gmail.com>
2017-12-12 15:28:13 -0500

    Revised pangram solution

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -1,15 +1,15 @@
 module.exports = class Pangram {
   constructor(string) {
     this.string = string;
-    this.alphabet = new Set([...'abcdefghijklmnopqrstuvwxyz']);
+    this.SIZE_OF_ALPHABET = 26;
+    this.ONLY_ALPHA_CHARACTERS = /[a-z]/g;
   }
 
   isPangram() {
-    return new Set(this. _onlyAlphaLetters()).size === this.alphabet.size;
+    return new Set(this._getAlphaCharactersFromString()).size === this.SIZE_OF_ALPHABET;
   }
 
-  _onlyAlphaLetters() {
-    return [...this.string.toLowerCase()]
-      .filter(letter => this.alphabet.has(letter));
+  _getAlphaCharactersFromString() {
+    return this.string.toLowerCase().match(this.ONLY_ALPHA_CHARACTERS);
   }
 }
