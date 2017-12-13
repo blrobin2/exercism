e37bbbb19b47dcd8dc798bfbb2008dd86fe7f469
Bennie <brobinson88@gmail.com>
2017-12-13 15:03:04 -0500

    Correct matrix solution

--- javascript/matrix/matrix.js
+++ javascript/matrix/matrix.js
@@ -1,8 +1,33 @@
 module.exports = class Matrix {
   constructor(matrixString) {
-    this.rows = matrixString.split("\n").map(rowString => rowString.split(' ').map(x => parseInt(x)));
-
-    this.columns = [[],[1903, 3, 4]];
-    
+    this.rows = this._matrixStringToRowMatrix(matrixString);
+    this.columns = this._rowsToColumns();
   }
-}
+
+  _matrixStringToRowMatrix(matrixString) {
+    return this._matrixStringToRowStrings(matrixString).map(
+      this._rowStringToArrayOfNumbers
+    );
+  }
+
+  _rowsToColumns() {
+    return this.rows.reduce(this._reduceRowsToColumns, []);
+  }
+
+  _matrixStringToRowStrings(matrixString) {
+    return matrixString.split("\n");
+  }
+
+  _rowStringToArrayOfNumbers(rowString) {
+    return rowString.split(" ").map(x => parseInt(x));
+  }
+
+  _reduceRowsToColumns(columns, row) {
+    row.forEach((cell, index) => {
+      if (!columns[index]) columns[index] = [];
+      columns[index].push(cell);
+    });
+
+    return columns;
+  }
+};
