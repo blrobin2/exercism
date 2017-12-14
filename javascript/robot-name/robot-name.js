14ab9d2f4e2055af53ce9ff2fbdc8692abaca7dd
Bennie <brobinson88@gmail.com>
2017-12-14 15:05:06 -0500

    Robot name first pass

---
+++ javascript/robot-name/robot-name.js
@@ -0,0 +1,38 @@
+const usedNames = {};
+
+module.exports = class Robot {
+  constructor() {
+    this.CHAR_CODE_A = "a".charCodeAt();
+    this.CHAR_CODE_Z = "z".charCodeAt();
+    this.name = this._constructName();
+  }
+
+  reset() {
+    this.name = this._constructName();
+  }
+
+  _constructName() {
+    let name;
+    while (usedNames[(name = this._generateString())]);
+    usedNames[name] = true;
+    return name;
+  }
+
+  _generateString() {
+    return this._randomLetter() + this._randomLetter() + this._randomNumber();
+  }
+
+  _randomLetter() {
+    return String.fromCharCode(
+      this._random(this.CHAR_CODE_A, this.CHAR_CODE_Z)
+    ).toUpperCase();
+  }
+
+  _randomNumber() {
+    return this._random(111, 999).toString();
+  }
+
+  _random(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+};
