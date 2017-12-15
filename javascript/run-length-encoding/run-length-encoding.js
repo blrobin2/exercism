709d762efa2d223298d66595444cfd1b5e7df018
Bennie <brobinson88@gmail.com>
2017-12-15 16:11:08 -0500

    Run length with documentation

--- javascript/run-length-encoding/run-length-encoding.js
+++ javascript/run-length-encoding/run-length-encoding.js
@@ -1,16 +1,22 @@
 module.exports = {
-  encode(string) {
-    // Grabs character as long as it's the same character, and
-    // last instance of character. That \1+ is what gives us the
-    // pair of string run and last instance of character. MAGIC!
-    return string.replace(/([A-z])\1+/g, (run, char) => run.length + char);
-  },
-  decode(string) {
-    // Grabs one or more numbers followed by a single letter
-    // Ignores first match, which is the whole group, and goes
-    // to the found number and character.
-    return string.replace(/(\d+)([A-z])/g, (_, runLength, char) =>
+  /**
+   * Grabs character as long as it's the same character, and
+   * last instance of character. That \1+ is what gives us the
+   * pair of string run and last instance of character. MAGIC!
+   * 
+   * @param {String} string 
+   */
+  encode: string =>
+    string.replace(/([A-z])\1+/g, (run, char) => run.length + char),
+  /**
+   * Grabs one or more numbers followed by a single letter
+   * Ignores first match, which is the whole group, and goes
+   * to the found number and character.
+   *
+   * @param {String} string
+   */
+  decode: string =>
+    string.replace(/(\d+)([A-z])/g, (_, runLength, char) =>
       char.repeat(runLength)
-    );
-  }
+    )
 };
