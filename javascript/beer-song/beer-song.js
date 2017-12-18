88f06bb8552bf0f30b4e628ee85c45198b3a95d0
Bennie <brobinson88@gmail.com>
2017-12-18 12:05:12 -0500

    Beer song, first pass

---
+++ javascript/beer-song/beer-song.js
@@ -0,0 +1,67 @@
+module.exports = class BeerSong {
+  constructor() {
+    this.FIRST_VERSE = 99;
+    this.LAST_VERSE = 0;
+  }
+
+  sing(fromVerse, toVerse = this.LAST_VERSE) {
+    let song = "";
+    for (let currVerse = fromVerse; currVerse >= toVerse; currVerse--) {
+      song += this._buildVerse(currVerse, toVerse);
+    }
+    return song;
+  }
+
+  verse(num) {
+    return (
+      `${this._firstLine(num)} on the wall, ${this._primaryLine(num)}.\n` +
+      `${this._secondaryLine(num)}, ` +
+      `${this._primaryLine(this._getNextNumber(num))} on the wall.\n`
+    );
+  }
+
+  _buildVerse(number, lastVerseNumber) {
+    const verse = this.verse(number);
+    return (number !== lastVerseNumber)
+      ? verse + "\n"
+      : verse;
+  }
+
+  _firstLine(num) {
+    return this._uppercase(this._primaryLine(num));
+  }
+
+  _primaryLine(num) {
+    return `${this._bottleCount(num)} ${this._pluralizeBottle(num)} of beer`;
+  }
+
+  _secondaryLine(number) {
+    return this._isLastVerse(number)
+      ? "Go to the store and buy some more"
+      : `Take ${this._howManyToTakeDown(number)} down and pass it around`;
+  }
+
+  _getNextNumber(number) {
+    return this._isLastVerse(number) ? this.FIRST_VERSE : number - 1;
+  }
+
+  _uppercase(string) {
+    return string[0].toUpperCase() + string.substring(1);
+  }
+
+  _bottleCount(number) {
+    return this._isLastVerse(number) ? "no more" : number.toString();
+  }
+
+  _pluralizeBottle(number) {
+    return number === 1 ? "bottle" : "bottles";
+  }
+
+  _howManyToTakeDown(number) {
+    return number === 1 ? "it" : "one";
+  }
+
+  _isLastVerse(number) {
+    return number === this.LAST_VERSE;
+  }
+};
