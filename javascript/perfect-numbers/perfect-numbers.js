7522bc3872d0c81c4e1197dad2502b3ee1975bda
Bennie <brobinson88@gmail.com>
2017-12-19 13:25:40 -0500

    perfect numbers - correct, but SLOW

---
+++ javascript/perfect-numbers/perfect-numbers.js
@@ -0,0 +1,30 @@
+module.exports = class PerfectNumbers {
+  constructor() {
+    this._PERFECT = "perfect";
+    this._ABUNDANT = "abundant";
+    this._DEFICIENT = "deficient";
+  }
+
+  classify(number) {
+    if (number <= 0) return "Classification is only possible for natural numbers.";
+
+    return this._getClassification(number);
+  }
+
+  _getClassification(number) {
+    const aliquotSum = this._getSumOfFactors(number);
+    if (aliquotSum === number) return this._PERFECT;
+    if (aliquotSum > number) return this._ABUNDANT;
+    return this._DEFICIENT;
+  }
+
+  _getSumOfFactors(number) {
+    let sum = 0;
+    for (let i = 1; i < number; i++) {
+      if (Number.isInteger(number / i)) {
+        sum += i;
+      }
+    }
+    return sum;
+  }
+};
