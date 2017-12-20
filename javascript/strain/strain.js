55e4958d2995da74bdb8e0c40dfb8a9010c5c9c6
Bennie <brobinson88@gmail.com>
2017-12-20 16:20:40 -0500

    Strain with revisions

--- javascript/strain/strain.js
+++ javascript/strain/strain.js
@@ -1,22 +1,15 @@
-module.exports = { 
+module.exports = {
   keep(list, predicate) {
     return this._filter(list, predicate);
   },
 
   discard(list, predicate) {
-    return this._filter(list, this._not(predicate));
+    return this._filter(list, elem => !predicate(elem));
   },
 
   _filter(list, predicate) {
     const newList = [];
-    for (let i = 0; i < list.length; i++)
-      if (predicate(list[i])) newList.push(list[i]);
+    for (let elem of list) if (predicate(elem)) newList.push(elem);
     return newList;
-  },
-
-  _not(predicate) {
-    return function() {
-      return !predicate.apply(null, arguments);
-    }
   }
 };
