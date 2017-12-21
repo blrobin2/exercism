145df1be8c5bfc07ab30ae242e45c74f7207eaa2
Bennie <brobinson88@gmail.com>
2017-12-21 08:54:58 -0500

    Accumulate - remove unnecessary early return

--- javascript/accumulate/accumulate.js
+++ javascript/accumulate/accumulate.js
@@ -1,5 +1,4 @@
 module.exports = function accumulate(list, operation) {
-  if (!list.length) return list;
   const newList = [];
   for (let elem of list) newList.push(operation(elem));
   return newList;
