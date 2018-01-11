656e5550d55fa65cb601367cfe1c56fd68862ffd
Bennie <brobinson88@gmail.com>
2018-01-11 09:30:22 -0500

    Hamming simpler solution

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -1,34 +1,21 @@
-import java.util.PrimitiveIterator.OfInt;
-
 class Hamming {
     private String leftStrand;
     private String rightStrand;
+    private int distance;
 
     Hamming(String leftStrand, String rightStrand) {
-        if (leftStrand.length() != rightStrand.length()) {
+        if (leftStrand.length() != rightStrand.length())
             throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
-        }
 
         this.leftStrand = leftStrand;
         this.rightStrand = rightStrand;
+        this.distance = 0;
     }
 
     int getHammingDistance() {
-        return countDistance(
-            getStrandIter(leftStrand),
-            getStrandIter(rightStrand)
-        );
-    }
-
-    private int countDistance(OfInt leftIter, OfInt rightIter) {
-        int distance = 0;
-        while (leftIter.hasNext() && rightIter.hasNext())
-            if (leftIter.next() != rightIter.next())
+        for (int i = 0; i < leftStrand.length(); i++)
+            if (leftStrand.charAt(i) != rightStrand.charAt(i))
                 distance++;
         return distance;
-    }
-
-    private OfInt getStrandIter(String strand) {
-        return strand.chars().iterator();
     }
 }
