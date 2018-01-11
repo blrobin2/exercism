3396d83247fd82a6e4f43747b7032947da143341
Bennie <brobinson88@gmail.com>
2018-01-11 08:39:44 -0500

    Pangram Checker with stream

--- java/pangram/src/main/java/PangramChecker.java
+++ java/pangram/src/main/java/PangramChecker.java
@@ -1,28 +1,10 @@
-import java.util.HashSet;
-
 public class PangramChecker {
     private final int LENGTH_OF_ALPHABET = 26;
 
     public boolean isPangram(String input) {
-        if (input.length() <= 0)
-            return false;
-        int letterCount = countLettersInInput(input);
-        return letterCount == LENGTH_OF_ALPHABET;
-    }
-
-    private int countLettersInInput(String input) {
-        HashSet<String> letters = new HashSet<>();
-        for (String character : getArrayOfCharacters(input))
-            if (isLetter(character))
-                letters.add(character);
-        return letters.size();
-    }
-
-    private String[] getArrayOfCharacters(String string) {
-        return string.toLowerCase().split("");
-    }
-
-    private boolean isLetter(String character) {
-        return Character.isLetter(character.charAt(0));
+        return input.toLowerCase().chars()
+            .distinct()
+            .filter(Character::isLetter)
+            .count() == LENGTH_OF_ALPHABET;
     }
 }
